17fc64954874047a165254de64756cb2f312cd75
wmm <2051046855@qq.com>
2017-09-03 19:46:01 +0800

    three commit

--- weekedproject/src/main/java/com/heihei/weekedproject/MainActivity.java
+++ weekedproject/src/main/java/com/heihei/weekedproject/MainActivity.java
@@ -6,38 +6,27 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     private MyTitleFit mytitlefit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        //找到在MainActivity的自定义布局
         mytitlefit = (MyTitleFit) findViewById(R.id.mytitlefit);
+        //调用它的接口,点击左边返回按钮让它Toast一些内容
         mytitlefit.SetleftOnClickListener(new MyTitleFit.leftOnClickListener() {
             @Override
             public void leftClick(View v) {
-
-                Toast.makeText(MainActivity.this, "你想要荒古?", Toast.LENGTH_SHORT).show();
-
-
+                Toast.makeText(MainActivity.this, "这是左边的按钮", Toast.LENGTH_SHORT).show();
             }
         });
-
+//调用它的接口,点击右边更多按钮让它Toast一些内容
         mytitlefit.SetrightOnClickListener(new MyTitleFit.rightOnClickListener() {
             @Override
             public void rightClick(View v) {
-
-
-                Toast.makeText(MainActivity.this, "简直活在梦里", Toast.LENGTH_SHORT).show();
-
+                Toast.makeText(MainActivity.this, "这是右边的按钮", Toast.LENGTH_SHORT).show();
             }
         });
-
-
     }
-
-
 }
--- weekedproject/src/main/java/com/heihei/weekedproject/MyTitleFit.java
+++ weekedproject/src/main/java/com/heihei/weekedproject/MyTitleFit.java
@@ -14,7 +14,7 @@ import android.widget.TextView;
 /**
  * Created by wmm on 2017/9/2 0002.
  */
-
+//创建MyTitleFit并继承LinearLayout
 public class MyTitleFit extends LinearLayout {
 
     private Button btn_return;
@@ -45,6 +45,7 @@ public class MyTitleFit extends LinearLayout {
 
     public MyTitleFit(Context context) {
         super(context);
+        //创建方法
         initView(context, null);
 
     }
@@ -52,18 +53,20 @@ public class MyTitleFit extends LinearLayout {
 
     public MyTitleFit(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        //创建方法
         initView(context, attrs);
 
     }
-
+    //初始化控件的方法
     private void initView(Context context, AttributeSet attrs) {
-
+        //找到关于标题的xml
         View inflate = inflate(context, R.layout.activity_title, this);
+       //依次找到控件,左右两边都是按钮中间是TextView
         btn_return = (Button) inflate.findViewById(R.id.btn_return);
         btn_more = (Button) inflate.findViewById(R.id.btn_more);
         tv_title = (TextView) inflate.findViewById(R.id.tv_title);
         titlefit = (LinearLayout) inflate.findViewById(R.id.titlefit);
-
+        //点击左边返回的按钮的监听,并调用接口
         btn_return.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,15 +74,11 @@ public class MyTitleFit extends LinearLayout {
                 if (leftOnClickListener != null) {
 
                     leftOnClickListener.leftClick(v);
-
-
                 }
-
-
             }
         });
 
-
+    //点击右边按钮的监听
         btn_more.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -87,19 +86,16 @@ public class MyTitleFit extends LinearLayout {
                 if (rightOnClickListener != null) {
 
                     rightOnClickListener.rightClick(v);
-
-
                 }
-
-
             }
         });
-
 
         if (attrs == null) {
             return;
         }
+        //初始化自定义属性的方法
         initattrs(context, attrs);
+        //把控件设置定义属性的方法
         setViewContent();
     }
 
@@ -108,40 +104,39 @@ public class MyTitleFit extends LinearLayout {
         if (attrs == null) {
             return;
         }
-
+        //找到自己定义的属性
         TypedArray typedarrat = context.obtainStyledAttributes(attrs, R.styleable.MyTitleFit);
+      //获得给标题定义的属性 文本  文字大小 文字颜色 背景颜色
         title = typedarrat.getString(R.styleable.MyTitleFit_title);
         titleSize = typedarrat.getDimensionPixelSize(R.styleable.MyTitleFit_titleSize, 16);
         titleColor = typedarrat.getColor(R.styleable.MyTitleFit_titleColor, Color.RED);
         bgColor = typedarrat.getColor(R.styleable.MyTitleFit_bgcolor, Color.GREEN);
-
+        //获得给左边返回按钮定义的属性 文本  文字大小 文字颜色 背景颜色
         btnleftSize = typedarrat.getDimensionPixelSize(R.styleable.MyTitleFit_btnleftSize, 12);
         btnlefttext = typedarrat.getString(R.styleable.MyTitleFit_btnlefttext);
         btnleftColor = typedarrat.getColor(R.styleable.MyTitleFit_btnleftcolor, Color.YELLOW);
         btnleftbgcolor = typedarrat.getDrawable(R.styleable.MyTitleFit_btnleftbgcolor);
-
+        //获得给右边更多按钮定义的属性 文本  文字大小 文字颜色 背景颜色
         btnrightSize = typedarrat.getDimensionPixelSize(R.styleable.MyTitleFit_btnrightSize, 14);
         btnrighttext = typedarrat.getString(R.styleable.MyTitleFit_btnrighttext);
         btnrightColor = typedarrat.getColor(R.styleable.MyTitleFit_btnrightcolor, Color.BLUE);
         btnrightbgcolor = typedarrat.getDrawable(R.styleable.MyTitleFit_btnrightbgcolor);
-
-
     }
 
     private void setViewContent() {
 
-
+        //给左边按钮附上默认属性  文本  文字大小 文字颜色 背景颜色
         btn_return.setText(btnlefttext);
         btn_return.setTextSize(btnleftSize);
         btn_return.setTextColor(btnleftColor);
         btn_return.setBackground(btnleftbgcolor);
 
-//tuolaji
+        //给右边按钮附上默认属性  文本  文字大小 文字颜色 背景颜色
         btn_more.setText(btnrighttext);
         btn_more.setTextSize(btnrightSize);
         btn_more.setTextColor(btnrightColor);
         btn_more.setBackground(btnrightbgcolor);
-
+        //给标题附上默认属性  文本  文字大小 文字颜色 背景颜色
         tv_title.setText(title);
         tv_title.setTextSize(titleSize);
         tv_title.setTextColor(titleColor);
@@ -150,7 +145,7 @@ public class MyTitleFit extends LinearLayout {
 
     }
 
-
+    //定义一个接口,实现点击左边返回按钮为了实现Toast等一些操作
     public interface leftOnClickListener {
 
         void leftClick(View v);
@@ -166,7 +161,7 @@ public class MyTitleFit extends LinearLayout {
 
     }
 
-
+    //定义一个接口,实现点击右边更多按钮为了实现Toast等一些操作
     public interface rightOnClickListener {
 
 
